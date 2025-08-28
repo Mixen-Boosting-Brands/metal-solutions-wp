b38c8ec50bff6163e2fee589026e6c88ff0b410c
Luis Carlos Pando <hey@luiscarlospando.com>
2025-08-28 16:36:14 -0600

    Implementar tema

--- footer.php
+++ footer.php
@@ -1,30 +1,293 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+        <footer class="py-60">
+            <div class="container">
+                <div class="row">
+                    <div
+                        class="col-md-4 my-auto order-md-2 mb-4 text-center"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                        data-aos-delay="0"
+                    >
+                        <a href="<?php echo esc_url(home_url()); ?>">
+                            <img
+                                class="logo img-fluid"
+                                src="<?php echo esc_url(
+                                    get_template_directory_uri(),
+                                ); ?>/assets/images/logo@2x.png"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div
+                        class="col-md-4 order-md-1 my-auto text-center text-md-start"
+                    >
+                        <nav
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-delay="100"
+                        >
+                            <ul class="list-unstyled mb-4 mb-md-0">
+                                <li class="mb-2">
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#jumbotron">Conócenos</a>
+                                </li>
+                                <li class="mb-2">
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#servicios">Servicios</a>
+                                </li>
+                                <li class="mb-2">
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#productos">Productos</a>
+                                </li>
+                                <li class="mb-2">
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#galeria-de-fotos">Conócenos</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#certificaciones"
+                                        >Certificaciones</a
+                                    >
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div
+                        class="col-md-4 order-md-3 my-auto text-center text-md-end"
+                    >
+                        <ul
+                            class="list-unstyled"
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-delay="300"
+                        >
+                            <li class="mb-3">
+                                <a
+                                    class="btn btn-primary rounded-pill"
+                                    href="tel:+526141735378"
+                                    ><i class="fa-solid fa-phone"></i> Karla
+                                    Solís 614 173 5378</a
+                                >
+                            </li>
+                            <li>
+                                <a
+                                    class="btn btn-primary rounded-pill"
+                                    href="tel:+526141776547"
+                                    ><i class="fa-solid fa-phone"></i> Marco
+                                    Beltrán 614 177 6547</a
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <p class="mb-0">
+                            Hecho con
+                            <i class="fa-solid fa-heart" alt="amor"></i> por
+                            <a href="https://mixen.mx/" target="_blank"
+                                >Mixen</a
+                            >
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo esc_html( date( 'Y' ) ); ?> Copyright <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'Powered by', 'html5blank' ); ?>
-					<a href="//wordpress.org" target="_blank">WordPress</a> &amp; <a href="//html5blank.com" target="_blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+        <!-- Modales -->
+        <div
+            class="modal fade"
+            id="contactModal"
+            tabindex="-1"
+            aria-labelledby="contactModalLabel"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="contactModalLabel">
+                            Formulario de Contacto
+                        </h5>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body">
+                        <div id="form-messages"></div>
+                        <form
+                            action="<?php echo esc_url(
+                                get_template_directory_uri(),
+                            ); ?>/mailer.php"
+                            method="POST"
+                            id="ajax-contact"
+                            class="needs-validation"
+                            novalidate
+                        >
+                            <div class="mb-3">
+                                <label for="nombre" class="form-label"
+                                    >Nombre*</label
+                                >
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="nombre"
+                                    name="nombre"
+                                    required
+                                />
+                                <div class="invalid-feedback">
+                                    Por favor ingresa tu nombre
+                                </div>
+                            </div>
 
-			</footer>
-			<!-- /footer -->
+                            <div class="mb-3">
+                                <label for="telefono" class="form-label"
+                                    >Teléfono*</label
+                                >
+                                <input
+                                    type="tel"
+                                    class="form-control"
+                                    id="telefono"
+                                    name="telefono"
+                                    pattern="[0-9]{10}"
+                                    required
+                                />
+                                <div class="invalid-feedback">
+                                    Por favor ingresa un número de teléfono
+                                    válido (10 dígitos)
+                                </div>
+                            </div>
 
-		</div>
-		<!-- /wrapper -->
+                            <div class="mb-3">
+                                <label for="email" class="form-label"
+                                    >Correo Electrónico*</label
+                                >
+                                <input
+                                    type="email"
+                                    class="form-control"
+                                    id="email"
+                                    name="email"
+                                    required
+                                />
+                                <div class="invalid-feedback">
+                                    Por favor ingresa un correo electrónico
+                                    válido
+                                </div>
+                            </div>
 
-		<?php wp_footer(); ?>
+                            <!-- div class="mb-3">
+                                <label for="mensaje" class="form-label"
+                                    >¿Cómo te podemos ayudar?*</label
+                                >
+                                <textarea
+                                    class="form-control"
+                                    id="mensaje"
+                                    rows="3"
+                                    required
+                                ></textarea>
+                                <div class="invalid-feedback">
+                                    Por favor cuéntanos cómo podemos ayudarte
+                                </div>
+                            </!div -->
 
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
+                            <div class="modal-footer">
+                                <button
+                                    type="button"
+                                    class="btn btn-secondary rounded-pill"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Cerrar
+                                </button>
+                                <button
+                                    type="submit"
+                                    class="btn btn-primary rounded-pill"
+                                >
+                                    Enviar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-	</body>
+        <div
+            class="modal fade"
+            id="elementoModal"
+            tabindex="-1"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body p-0">
+                        <!-- This will get injected -->
+                        <div class="elemento-modal"></div>
+                    </div>
+                    <div class="modal-footer border-0">
+                        <button
+                            type="button"
+                            class="btn btn-primary rounded-pill"
+                            data-bs-dismiss="modal"
+                        >
+                            Cerrar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div
+            class="modal fade"
+            id="galeriaModal"
+            tabindex="-1"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog modal-dialog-centered modal-xl">
+                <div class="modal-content bg-dark border-0">
+                    <div class="modal-body p-0 position-relative">
+                        <div class="swiper swiper-modal-galeria">
+                            <div class="swiper-wrapper text-center">
+                                <!-- Populate these dynamically -->
+                            </div>
+
+                            <div class="swiper-button-prev"></div>
+                            <div class="swiper-button-next"></div>
+                        </div>
+                        <button
+                            type="button"
+                            class="btn-close position-absolute top-0 end-0 m-3 btn-close-white"
+                            data-bs-dismiss="modal"
+                        ></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php wp_footer(); ?>
+
+        <script src="<?php echo esc_url(
+            get_template_directory_uri(),
+        ); ?>/assets/js/app.bundle.js"></script>
+
+        <!-- Google tag (gtag.js) -->
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-J0CXD3ETHD"
+        ></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+
+            gtag("config", "G-J0CXD3ETHD");
+        </script>
+    </body>
 </html>
--- header.php
+++ header.php
@@ -1,51 +1,201 @@
 <!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo("charset"); ?>" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+            name="description"
+            content="<?php bloginfo("description"); ?>"
+        />
+        <title>
+            <?php
+            wp_title("");
+            if (wp_title("", false)) {
+                echo " : ";
+            }
+            bloginfo("name");
+            ?>
+        </title>
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/favicon.ico" rel="shortcut icon">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
+        <link rel="alternate" type="application/rss+xml" title="<?php bloginfo(
+            "name",
+        ); ?>" href="<?php bloginfo("rss2_url"); ?>" />
+        <link
+            rel="icon"
+            type="image/png"
+            href="<?php echo esc_url(
+                get_template_directory_uri(),
+            ); ?>/favicon-96x96.png"
+            sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="<?php echo esc_url(
+            get_template_directory_uri(),
+        ); ?>/favicon.svg" />
+        <link rel="shortcut icon" href="<?php echo esc_url(
+            get_template_directory_uri(),
+        ); ?>/favicon.ico" />
+        <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="<?php echo esc_url(
+                get_template_directory_uri(),
+            ); ?>/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Metal Solutions" />
+        <link rel="manifest" href="/site.webmanifest" />
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+        <link rel="stylesheet" href="<?php echo esc_url(
+            get_template_directory_uri(),
+        ); ?>/assets/css/styles.css" />
 
-		<?php wp_head(); ?>
-		<script>
-		// conditionizr.com
-		// configure environment tests
-		conditionizr.config({
-			assets: '<?php echo esc_url( get_template_directory_uri() ); ?>',
-			tests: {}
-		});
-		</script>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <div id="backdrop"></div>
+        <div class="menu">
+            <a id="cerrar-menu" href="javascript:void(0)">
+                <i class="fas fa-times"></i>
+            </a>
+            <div class="menu-contenido">
+                <a class="anchor" id="btn-logo" href="<?php echo esc_url(
+                    home_url(),
+                ); ?>">
+                    <img
+                        class="logo img-fluid"
+                        src="<?php echo esc_url(
+                            get_template_directory_uri(),
+                        ); ?>/assets/images/logo@2x.png"
+                        alt=""
+                    />
+                </a>
+                <nav>
+                    <ul id="navmenu" class="list-unstyled mb-0">
+                        <li>
+                            <a class="anchor" id="btn-nav-1" href="<?php echo esc_url(
+                                home_url(),
+                            ); ?>#jumbotron"
+                                >Inicio</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-2" href="<?php echo esc_url(
+                                home_url(),
+                            ); ?>#servicios"
+                                >Servicios</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-3" href="<?php echo esc_url(
+                                home_url(),
+                            ); ?>#productos"
+                                >Productos</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="anchor"
+                                id="btn-nav-4"
+                                href="<?php echo esc_url(
+                                    home_url(),
+                                ); ?>#galeria-de-fotos"
+                                >Galería</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                data-bs-toggle="modal"
+                                data-bs-target="#contactModal"
+                                >Contáctanos</a
+                            >
+                        </li>
+                    </ul>
+                </nav>
+                <div id="contacto-menu">
+                    <ul class="list-unstyled">
+                        <li class="mb-3">
+                            <a
+                                class="btn btn-primary rounded-pill"
+                                href="tel:+526141735378"
+                                ><i class="fa-solid fa-phone"></i> Karla Solís
+                                614 173 5378</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="btn btn-primary rounded-pill"
+                                href="tel:+526141776547"
+                                ><i class="fa-solid fa-phone"></i> Marco Beltrán
+                                614 177 6547</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
 
-	</head>
-	<body <?php body_class(); ?>>
-
-		<!-- wrapper -->
-		<div class="wrapper">
-
-			<!-- header -->
-			<header class="header clear" role="banner">
-
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo esc_url( home_url() ); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
-
-			</header>
-			<!-- /header -->
+        <header id="navbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 col-lg-4 my-auto">
+                        <a href="<?php echo esc_url(home_url()); ?>">
+                            <img
+                                src="<?php echo esc_url(
+                                    get_template_directory_uri(),
+                                ); ?>/assets/images/logo@2x.png"
+                                alt=""
+                                class="logo img-fluid"
+                                id="logo-navbar"
+                            />
+                        </a>
+                    </div>
+                    <div class="col-lg-4 d-none d-lg-block my-auto text-center">
+                        <nav>
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item">
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#jumbotron">Inicio</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#servicios">Servicios</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#productos">Productos</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="<?php echo esc_url(
+                                        home_url(),
+                                    ); ?>#galeria-de-fotos">Galería</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-6 col-lg-4 my-auto text-end">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item d-none d-lg-block">
+                                <a
+                                    class="btn btn-primary rounded-pill"
+                                    href="#"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#contactModal"
+                                >
+                                    Contáctanos
+                                </a>
+                            </li>
+                            <li class="list-inline-item d-lg-none">
+                                <a id="mburger" href="javascript:void(0)">
+                                    <i class="fas fa-bars"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr />
+        </header>
